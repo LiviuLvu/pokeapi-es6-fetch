d9cf2ea78f0b720b09c9b86016f3f6ceea99ad85
Liviu Iancu <liviuiancu@gmx.com>
2016-08-22 19:05:38 +0300

    Completed first 15 minutes of tutorial

--- source/main.js
+++ source/main.js
@@ -1,3 +1,60 @@
-jQuery(document).ready(function($) {
-	//testing
+'use strict';
+// continue from 14:23
+
+const fetchOptions = {
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	// accept cross origin resources
+	mode: 'cors'
+}
+
+$('form').on('submit', function(e) {
+	e.preventDefault();
+	let types = $('input[type=text]').val().replace(/\s/g, '');
+	types = types.split(',');
+	// map over our types array, return fetch calls
+	let trainerTypeCalls = types.map(elem => {
+		// configuration object
+		// fetch returns a promise
+		return fetch(`http://pokeapi.co/api/v2/type/${elem}/`, fetchOptions)
+	});
+	// when fetch resolves it will return a response object
+	Promise.all(trainerTypeCalls)
+		.then(data => {
+			data = data.map(singleData => singleData.json());
+			Promise.all(data)
+				.then(res => {
+					console.log(res);
+				});
+		});
 });
+
+// takes array of fetch calls
+function getPromiseData(promisesArray) {
+	return new Promise((resolve, reject) => {
+		// call promise.all on array of fetch calls
+		Promise.all(promisesArray)
+			// get json data
+			.then(res => {
+				return res.map(type => type.json());
+			})
+			// array of json calls passed to callback
+			.then(res => {
+				Promise.all(res).then(resolve);
+			})
+			.catch(reject);
+	})
+}
+
+function displayPokemon(pokemon) {
+	// loop through and display the pokemon
+	pokemon.forEach(function(poke) {
+		let $container = $('<div>').addClass('pokemon');
+		let $image = $('<img>').attr('src', `http://pokeapi.co/media/img/${poke.id}.png`);
+		let $title = $('<h2>').text(poke.name);
+		$container.append($image, $title);
+		$('.poke-container').append($container);
+	});
+}
+
